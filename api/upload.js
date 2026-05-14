ee1e1a95b6d21d4d69ec485cb3910d3f94125395
dovalent <dovanaz123@gmail.com>
2026-05-15 01:46:04 +0700

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -25,8 +25,10 @@ function combineCookies(a, b) {
 }
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
-  
   try {
     const body = req.body;
     if (!body || !body.session || !body.image) {
